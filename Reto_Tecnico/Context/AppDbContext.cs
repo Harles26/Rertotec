04c6924d3de8068533c846fd93e76cf431b5cf32
Harles <u192005740@utp.edu.pe>
2024-12-22 20:30:44 -0500

    Agregar archivos de proyecto.

---
+++ Reto_Tecnico/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reto_Tecnico.Models;
+
+namespace Reto_Tecnico.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<Clientes>Cliente {  get; set; }
+    }
+}
---
+++ Reto_Tecnico/Models/Clientes.cs
@@ -0,0 +1,10 @@
+﻿namespace Reto_Tecnico.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; } = string.Empty;
+        public required string Apellidos { get; set; } = string.Empty;
+        public required string FechaNacimiento { get; set; } = string.Empty;
+    }
+}
